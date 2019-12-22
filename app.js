4ff95385a1c13eb9bdacd128f5955782d439ec62
Vivekanand P V <vivekanandpv@outlook.com>
2019-12-22 16:31:19 +0530

    done

--- app.js
+++ app.js
@@ -2,8 +2,32 @@ const express = require('express');
 
 const app = express();
 
-app.use('/', (req, res) => {
-  res.send({ message: 'From express', on: new Date() });
+const baseRouter = express.Router();
+
+baseRouter.get('/api/items', (req, res) => {
+  res.send({ message: 'Get From express', on: new Date() });
 });
+
+baseRouter.get('/api/items/:id', (req, res) => {
+  res.send({ message: 'Get From express', on: new Date() });
+});
+
+baseRouter.get('/api/items/:id/:year', (req, res) => {
+  res.send({ message: 'Get From express', on: new Date(), params: req.params });
+});
+
+baseRouter.post('/api/items', (req, res) => {
+  res.send({ message: 'Post From express', on: new Date() });
+});
+
+baseRouter.put('/api/items', (req, res) => {
+  res.send({ message: 'Put From express', on: new Date() });
+});
+
+baseRouter.delete('/api/items', (req, res) => {
+  res.send({ message: 'Delete From express', on: new Date() });
+});
+
+app.use('/', baseRouter);
 
 app.listen(3000);
